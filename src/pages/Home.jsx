4ba6eddc9fb44537a82434f77ed07ad12d45148c
kannangan <kannanganesan1996@gmail.com>
2021-06-09 13:16:45 +0200

    success

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import SearchBar from "../components/SearchBar";
+import CardsList from "../components/CardsList";
+
+const Home = () => {
+  const [terms, setTerms] = useState([]);
+
+  const addTerm = term => {
+    setTerms([...new Set([...terms, term])]);
+  };
+
+  const clear = term => {
+    if (term === "") setTerms([]);
+    else setTerms(terms.filter(t => t !== term));
+  };
+
+  return (
+    <div className="Home">
+      <div className="header">
+      </div>
+      <div className="body">
+        <SearchBar terms={terms} clear={clear} />
+        <CardsList terms={terms} addTerm={addTerm} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
